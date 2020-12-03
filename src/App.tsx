89e238a7ac40910bdef1bb5b840a342fbb7f8145
Guy Tepper <guytepper@gmail.com>
2020-12-03 14:32:56 +0200

    Added restyle lib

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import { View, Text } from 'react-native';
 import { NavigationContainer, RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ThemeProvider } from '@shopify/restyle';
+import theme from './theme';
 import Icon from 'react-native-vector-icons/Feather';
 import { Events } from './screens';
-
 type RootStackParamList = {
   Home: undefined;
 };
@@ -30,52 +31,54 @@ const Tab = createBottomTabNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
 
-            switch (route.name) {
-              case 'Events':
-                iconName = 'calendar';
-                break;
+              switch (route.name) {
+                case 'Events':
+                  iconName = 'calendar';
+                  break;
 
-              case 'Map':
-                iconName = 'map';
-                break;
+                case 'Map':
+                  iconName = 'map';
+                  break;
 
-              case 'CheckIn':
-                iconName = 'map-pin';
-                break;
+                case 'CheckIn':
+                  iconName = 'map-pin';
+                  break;
 
-              case 'Inbox':
-                iconName = 'inbox';
-                break;
-              case 'Profile':
-                iconName = 'user';
-                break;
-            }
+                case 'Inbox':
+                  iconName = 'inbox';
+                  break;
+                case 'Profile':
+                  iconName = 'user';
+                  break;
+              }
 
-            if (focused) {
-            }
+              if (focused) {
+              }
 
-            return <Icon name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: '#6E7DFF',
-          inactiveTintColor: 'gray',
-          showLabel: false,
-        }}
-      >
-        <Tab.Screen name="Map" component={HomeScreen} options={{ title: 'מפה' }} />
-        <Tab.Screen name="Events" component={Events} />
-        <Tab.Screen name="CheckIn" component={HomeScreen} />
-        <Tab.Screen name="Inbox" component={HomeScreen} />
-        <Tab.Screen name="Profile" component={HomeScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: '#6E7DFF',
+            inactiveTintColor: 'gray',
+            showLabel: false,
+          }}
+        >
+          <Tab.Screen name="Map" component={HomeScreen} options={{ title: 'מפה' }} />
+          <Tab.Screen name="Events" component={Events} />
+          <Tab.Screen name="CheckIn" component={HomeScreen} />
+          <Tab.Screen name="Inbox" component={HomeScreen} />
+          <Tab.Screen name="Profile" component={HomeScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
 
