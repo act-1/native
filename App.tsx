17f2ad513e1ec6f5ecf6a6d8c2b9f6fd0e20dadc
Guy Tepper <guytepper@gmail.com>
2020-12-03 06:47:11 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, ScrollView, View, Text} from 'react-native';
+
+function App() {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>Hi!</Text>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
