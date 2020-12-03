dbc274f941e9d439811b40b9455f344207e4a053
Guy Tepper <guytepper@gmail.com>
2020-12-03 12:06:36 +0200

    Added navigation bar w/ icons

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'react-native/no-inline-styles': 0,
+    'capitalized-comments': [
+      'error',
+      'always',
+      {
+        ignorePattern: 'import|export|const|if|useEffect',
+        ignoreConsecutiveComments: true,
+      },
+    ],
+    'spaced-comment': ['error', 'always', { exceptions: ['*'], markers: ['*'] }],
+    'keyword-spacing': ['error', { before: true }],
+  },
 };
--- android/app/src/main/java/com/codenamemerry/MainApplication.java
+++ android/app/src/main/java/com/codenamemerry/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.react.modules.i18nmanager.I18nUtil;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -43,6 +44,9 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
+    sharedI18nUtilInstance.forceRTL(this, true);
+    sharedI18nUtilInstance.allowRTL(this, true);
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
