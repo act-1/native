358dcedf315090971642529e36dfbe83b4437cb0
Guy Tepper <guytepper@gmail.com>
2020-12-04 21:00:03 +0200

    Removed forceRTL statement
    Not needed(?)

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-import { AppRegistry, I18nManager } from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
-
-I18nManager.forceRTL(true);
 
 AppRegistry.registerComponent(appName, () => App);
