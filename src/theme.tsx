b962d1b785b87ce3e18dbd45eb254f225bc5d2ad
Guy Tepper <guytepper@gmail.com>
2020-12-04 07:45:46 +0200

    Update theme.tsx

--- src/theme.tsx
+++ src/theme.tsx
@@ -9,24 +9,49 @@ const palette = {
   greenPrimary: '#0ECD9D',
   greenDark: '#0A906E',
 
+  redPrimary: '#E03616',
+
   black: '#0B0B0B',
   white: '#F0F2F3',
+  grey: '#737373',
 };
 
 const theme = createTheme({
   colors: {
+    primaryText: palette.black,
+    lightText: palette.grey,
     mainBackground: palette.white,
     cardPrimaryBackground: palette.purplePrimary,
+    eventBoxDateColor: palette.redPrimary,
   },
   spacing: {
+    xs: 4,
     s: 8,
-    m: 16,
+    m: 12,
+    xm: 16,
     l: 24,
     xl: 40,
   },
   breakpoints: {
     phone: 0,
     tablet: 768,
+  },
+  textVariants: {
+    eventBoxTitle: {
+      fontWeight: 'bold',
+      fontSize: 17,
+      color: 'primaryText',
+    },
+    eventBoxDate: {
+      fontWeight: 'bold',
+      fontSize: 13,
+      color: 'eventBoxDateColor',
+    },
+    eventBoxLocation: {
+      fontWeight: '400',
+      fontSize: 15,
+      color: 'lightText',
+    },
   },
 });
 
