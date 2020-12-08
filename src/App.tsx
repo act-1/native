70362dfa913ea613497ee409c772005468c4ac96
Guy Tepper <guytepper@gmail.com>
2020-12-08 08:40:32 +0200

    Removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { View, Text } from 'react-native';
-import { DefaultTheme, NavigationContainer, RouteProp } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from '@shopify/restyle';
 import theme from './theme';
 import AppNavigator from './navigations/AppNavigator';
-
-type RootStackParamList = {
-  Home: undefined;
-};
-
-type HomeRouteProps = RouteProp<RootStackParamList, 'Home'>;
-type HomeScreenProps = StackNavigationProp<RootStackParamList, 'Home'>;
-type HomeProps = {
-  route: HomeRouteProps;
-  navigation: HomeScreenProps;
-};
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 
 function App() {
   return (
