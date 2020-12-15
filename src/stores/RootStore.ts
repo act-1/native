897e6ca21b5bbe51d00eb85fda490d9ea77f42c3
Guy Tepper <guytepper@gmail.com>
2020-12-15 09:05:25 +0200

    Added firebase auth

--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,19 +1,40 @@
 import { makeAutoObservable, runInAction } from 'mobx';
 import { getEventList } from '../api/events';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 import { IEvent } from '@types/event';
+
+// TODO: Create AuthStore and EventStore
 
 class RootStore {
   events: IEvent[] | [] = [];
+  initializing = true;
+  user: FirebaseAuthTypes.User | null = null;
 
   constructor() {
     makeAutoObservable(this);
     this.getEvents();
+
+    auth().onAuthStateChanged((user) => {
+      this.user = user;
+    });
+
+    this.signInAnonymously();
+  }
+
+  signInAnonymously() {
+    auth()
+      .signInAnonymously()
+      .then(() => {
+        console.log('User signed in anonymously');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   async getEvents() {
     try {
       const events = await getEventList();
-      console.log('RootStore events:', events);
       runInAction(() => {
         this.events = events;
       });
