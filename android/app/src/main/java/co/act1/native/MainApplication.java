93dcf75c8233a705e8a4d7a8a11acc267ad70bb4
Guy Tepper <guytepper@gmail.com>
2020-12-16 11:52:41 +0200

    Renamed app

--- android/app/src/main/java/co/act1/native/MainApplication.java
+++ android/app/src/main/java/co/act1/native/MainApplication.java
@@ -1,4 +1,4 @@
-package com.codenamemerry;
+package co.act1.native;
 
 import android.app.Application;
 import android.content.Context;
@@ -66,7 +66,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.codenamemerry.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("co.act1.native.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
