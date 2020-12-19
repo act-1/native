28bfbf731996c2f7ae961242cf010db057dd098a
Guy Tepper <guytepper@gmail.com>
2020-12-19 20:40:09 +0200

    Fixed emulator url

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ import App from './src/App';
 import { name as appName } from './app.json';
 
 // Use a local emulator in development
-// if (__DEV__) {
-// console.log(__DEV__);
-// functions().useFunctionsEmulator('http://localhost:5001/codename-merry/us-central1/attendEvent');
-// }
+if (__DEV__) {
+  functions().useFunctionsEmulator('http://localhost:5001');
+}
 
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
   console.log('Message handled in the background!', remoteMessage);
