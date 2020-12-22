8a644649e9806ab0da980b5214a05a95a7b8914d
Guy Tepper <guytepper@gmail.com>
2020-12-22 07:52:09 +0200

    Create EventStore

--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,25 +1,24 @@
-import { makeAutoObservable, runInAction } from 'mobx';
-import { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
+import { makeAutoObservable } from 'mobx';
 import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 import EventsAPI from '../api/events';
 import { createAnonymousUser } from '../api/user';
-import { IEvent } from '@types/event';
+import eventStore from './EventStore';
 
 // TODO: Create AuthStore and EventStore
 
 class RootStore {
-  events: IEvent[] | [] = [];
   initializing = true;
   user: FirebaseAuthTypes.User | null = null;
   userEventIds: string[] = [];
+  eventStore: null | eventStore = null;
 
   constructor() {
     makeAutoObservable(this);
+    this.eventStore = new eventStore(this);
 
     auth().onAuthStateChanged((user: FirebaseAuthTypes.User | null) => {
       if (user) {
         this.user = user;
-        this.getEvents();
         EventsAPI.getUserEvents(user.uid).then((events) => {
           this.userEventIds = events;
         });
@@ -33,50 +32,6 @@ class RootStore {
       await createAnonymousUser(user.uid);
     } catch (err) {
       console.error(err);
-    }
-  }
-
-  async getEvents() {
-    try {
-      const events = await EventsAPI.getEventList();
-      runInAction(() => {
-        this.events = events;
-      });
-      return events;
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  async attendEvent({
-    eventId,
-    type,
-    eventDate,
-  }: {
-    eventId: string;
-    type: string;
-    eventDate?: FirebaseFirestoreTypes.Timestamp;
-  }): Promise<{ attended?: boolean; removed?: boolean } | undefined> {
-    try {
-      let result;
-
-      if (type === 'attend' && eventDate) {
-        result = await EventsAPI.attendEvent({ eventId, eventDate });
-        this.userEventIds.push(eventId);
-      }
-      if (type === 'remove') {
-        if (this.userEventIds.includes(eventId)) {
-          result = await EventsAPI.attendenceRemoval({ eventId });
-          const updatedUserEventIds = this.userEventIds.filter((id) => id !== eventId);
-          runInAction(() => {
-            this.userEventIds = updatedUserEventIds;
-          });
-        }
-      }
-
-      return result;
-    } catch (err) {
-      throw err;
     }
   }
 }
