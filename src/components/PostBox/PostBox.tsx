bffa89f2968febe287d814d6d1cb497a024afd50
Guy Tepper <guytepper@gmail.com>
2020-12-25 03:18:48 +0200

    Added PostFeed component

--- src/components/PostBox/PostBox.tsx
+++ src/components/PostBox/PostBox.tsx
@@ -5,25 +5,33 @@ import FastImage from 'react-native-fast-image';
 import Icon from 'react-native-vector-icons/Feather';
 import { Box, Text } from '../../components';
 
-function PostBox() {
+type PostBoxProps = {
+  authorName: string;
+  authorPicture: URL;
+  content: string;
+  image?: URL;
+  timestamp: string;
+};
+
+function PostBox(props: PostBoxProps) {
+  const { authorName, authorPicture, content, timestamp } = props;
   return (
     <Box backgroundColor="mainBackground" alignItems="flex-start" padding="m">
       <Box flexDirection="row" marginBottom="s">
         <FastImage
           source={{
-            uri:
-              'https://scontent.ftlv16-1.fna.fbcdn.net/v/t1.0-9/90528969_110795007223055_4530916542791024640_o.png?_nc_cat=106&ccb=2&_nc_sid=09cbfe&_nc_ohc=-OTfEcMcxCQAX_LIlQg&_nc_ht=scontent.ftlv16-1.fna&oh=49ab9c3dec2ca614c297f85099ccf150&oe=600CB4D8',
+            uri: authorPicture.href,
           }}
           style={styles.authorImage}
         />
         <Box alignItems="flex-start">
-          <Text variant="boxTitle">הדגלים השחורים</Text>
+          <Text variant="boxTitle">{authorName}</Text>
           <Text variant="boxSubtitle">לפני 4 שעות</Text>
         </Box>
       </Box>
       <Box alignItems="flex-start" marginBottom="m">
         <Text variant="text" fontFamily="Arial">
-          המחאה פירקה את הליכוד. ובעזרת השם גם תפרק את הממשלה הכושלת הזו עוד הלילה.
+          {content}
         </Text>
       </Box>
       <Box width="100%" height={2} backgroundColor="seperator" marginBottom="m" />
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Text, Box, PostBox } from '../../components';
+import { ScrollView, StyleSheet } from 'react-native';
+import { Box, PostBox } from '../../components';
+import PostFeed from './PostFeed';
+
+const posts = [1, 2, 3];
 
 const HomeHeader = () => {
-  return <Box style={styles.oval} />;
+  return <Box style={styles.header} />;
 };
 
 function Home() {
   return (
-    <Box flex={1} width="100%" backgroundColor="dimmedBackground">
+    <ScrollView>
       <HomeHeader />
-      <PostBox />
-    </Box>
+      <PostFeed posts={posts} />
+    </ScrollView>
   );
 }
 
 export default Home;
 
 const styles = StyleSheet.create({
-  oval: {
-    marginTop: -50,
+  header: {
+    flex: 1,
     width: '100%',
     height: 175,
     backgroundColor: '#304BFF',
