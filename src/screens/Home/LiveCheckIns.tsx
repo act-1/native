76085833d231793ef117dbbfd233778c416d0a64
Guy Tepper <guytepper@gmail.com>
2020-12-26 13:12:39 +0200

    Random numbers

--- src/screens/Home/LiveCheckIns.tsx
+++ src/screens/Home/LiveCheckIns.tsx
@@ -5,10 +5,10 @@ function LiveCheckIns() {
   return (
     <Box>
       <Box marginBottom="m">
-        <LocationBox name="כיכר פריז" address="ירושלים" counter={'3,212'} />
+        <LocationBox name="כיכר פריז" address="ירושלים" counter={'3,412'} />
         <LocationBox name="כיכר רבין" address="תל אביב" counter={'1,421'} />
         <LocationBox name="בית הנאשם" address="קיסריה" counter={'981'} />
-        <LocationBox name="צומת פרדסיה" address="פרדסיה" counter={'50'} />
+        <LocationBox name="צומת פרדסיה" address="פרדסיה" counter={'59'} />
       </Box>
       <Text variant="appLink" textAlign="center">
         לרשימה המלאה{' >'}
