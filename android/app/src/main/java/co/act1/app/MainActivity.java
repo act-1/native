dd683768b0050f61ceec9dbaee00590d36cc84cf
Guy Tepper <guytepper@gmail.com>
2020-12-30 18:50:00 +0200

    Import order fixes on MainActivity.java

--- android/app/src/main/java/co/act1/app/MainActivity.java
+++ android/app/src/main/java/co/act1/app/MainActivity.java
@@ -1,17 +1,12 @@
-import android.os.Bundle; 
 package co.act1.app;
+
+import android.os.Bundle; 
 
 import com.facebook.react.ReactActivity;
 import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); // <- display the generated bootsplash.xml drawable over our MainActivity
-  }
-  
-  /**
+ /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
@@ -20,5 +15,9 @@ public class MainActivity extends ReactActivity {
     return "Act1";
   }
 
-  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); 
+  }  
 }
