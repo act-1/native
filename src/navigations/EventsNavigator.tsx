c38f0d77e4d7e68a7f8fa478a86f94778ead48b8
Guy Tepper <guytepper@gmail.com>
2021-01-06 12:16:54 +0200

    Removed unused import

--- src/navigations/EventsNavigator.tsx
+++ src/navigations/EventsNavigator.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { EventList, EventPage } from '../screens';
+import { EventList } from '../screens';
 
 const EventsStack = createStackNavigator();
 
