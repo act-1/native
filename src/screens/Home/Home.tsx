cb0b10b0fc73b91b8dae518eb14937aa1e164bab
Guy Tepper <guytepper@gmail.com>
2021-01-08 08:36:52 +0200

    Hoem component cleanup

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,46 +1,21 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import { StatusBar, SafeAreaView, ScrollView, StyleSheet } from 'react-native';
-import { Text } from '../../components';
-import { useStore } from '../../stores';
+import { View, StatusBar, StyleSheet } from 'react-native';
 import PostFeed from './PostFeed';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { HomeScreenProps } from '@types/navigation';
 
-const HomeHeader = () => {
-  const insets = useSafeAreaInsets();
-
-  return <StatusBar backgroundColor="#fafafa" barStyle="dark-content" networkActivityIndicatorVisible={false} />;
-  // return (
-  //   <SafeAreaView style={[styles.header, { height: 46 + insets.top }]}>
-  //     <Text variant="hugeTitle" fontFamily="Rubik-Medium" fontWeight="500" fontSize={18} style={{ color: '#6E7DFF' }}>
-  //       Act1
-  //     </Text>
-  //   </SafeAreaView>
-  // );
-};
-
-function Home({ navigation }: HomeScreenProps) {
+function Home() {
   return (
-    <>
-      <HomeHeader />
+    <View style={styles.homeWrapper}>
+      <StatusBar backgroundColor="#fafafa" barStyle="dark-content" networkActivityIndicatorVisible={false} />
       <PostFeed />
-    </>
+    </View>
   );
 }
 
 export default observer(Home);
 
 const styles = StyleSheet.create({
-  header: {
-    width: '100%',
-    height: 92,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fafafa',
-    borderBottomColor: '#ebebeb',
-    borderBottomWidth: 1,
-    elevation: 5,
-    zIndex: 2,
+  homeWrapper: {
+    flex: 1,
   },
 });
