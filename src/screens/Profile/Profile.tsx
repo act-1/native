f5329a614a4a97e42d118087798b7ee73f8ed255
Guy Tepper <guytepper@gmail.com>
2021-01-08 20:06:13 +0200

    Move facebook login method to utils file

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -4,74 +4,14 @@ import { ProfileScreenProps } from '../../types/navigation';
 import { Box } from '../../components';
 import { RoundedButton } from '../../components/Buttons';
 
-import auth from '@react-native-firebase/auth';
-import { LoginManager, AccessToken, GraphRequest, GraphRequestManager } from 'react-native-fbsdk';
-
-// Create response callback.
-function _responseInfoCallback(error: any, result: any) {
-  if (error) {
-    console.log('Error fetching data: ', error);
-  } else {
-    console.log('Success fetching data: ', result);
-  }
-}
-
-async function onFacebookButtonPress() {
-  try {
-    // Attempt login with permissions
-    const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
-    console.log(result);
-    // if (result.isCancelled) {
-    //   throw 'User cancelled the login process';
-    // }
-
-    // // Once signed in, get the users AccesToken
-    const data = await AccessToken.getCurrentAccessToken();
-    console.log(data);
-    // if (!data) {
-    //   throw 'Something went wrong obtaining access token';
-    // }
-
-    // // Create a Firebase credential with the AccessToken
-    // const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
-
-    // console.log(facebookCredential);
-
-    // const userCredential = await auth().currentUser?.linkWithCredential(facebookCredential);
-    // const user = await auth().currentUser;
-    // console.log(user);
-    // const facebookid = user?.providerData[0].uid;
-    // console.log(user?.providerData[0].uid);
-
-    // // const facebookUserId = userCredential?.additionalUserInfo?.profile?.id;
-    // const tokenObj = await AccessToken.getCurrentAccessToken();
-    // // Create a graph request asking for user information with a callback to handle the response.
-    // const infoRequest = new GraphRequest(
-    //   `/me`,
-    //   {
-    //     accessToken: tokenObj.accessToken,
-    //     parameters: {
-    //       fields: {
-    //         string: 'first_name,gender,picture.type(large)',
-    //       },
-    //     },
-    //   },
-    //   _responseInfoCallback
-    // );
-    // // Start the graph request.
-    // new GraphRequestManager().addRequest(infoRequest).start();
-  } catch (err) {
-    console.log(err);
-  }
-}
+import { facebookLogin } from '@utils/auth-utils';
 
 function Profile({ navigation }: ProfileScreenProps) {
   return (
     <Box paddingTop="xl" justifyContent="center" alignItems="center">
-      <RoundedButton text="התחברות דרך פייסבוק" color="black" onPress={onFacebookButtonPress} />
+      <RoundedButton text="התחברות דרך פייסבוק" color="black" onPress={facebookLogin} />
     </Box>
   );
-  // return <Button title="open me" onPress={() => navigation.navigate('SignUpNavigator', { screen: 'SignUpScreen' })} />;
 }
 
 export default Profile;
---
+++ src/utils/auth-utils.ts
@@ -0,0 +1,72 @@
+import crashlytics from '@react-native-firebase/crashlytics';
+import auth from '@react-native-firebase/auth';
+import { LoginManager, AccessToken, GraphRequest, GraphRequestManager } from 'react-native-fbsdk';
+
+type GraphAPIResult = {
+  picture: {
+    data: {
+      url: string;
+    };
+  };
+};
+
+export async function facebookLogin() {
+  try {
+    // Attempt login with permissions
+    const loginRequest = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+
+    if (loginRequest.isCancelled) {
+      throw new Error('User cancelled the login process');
+    }
+
+    // Once signed in, get the users AccesToken
+    const token = await AccessToken.getCurrentAccessToken();
+
+    if (!token) {
+      throw new Error('Something went wrong obtaining access token');
+    }
+
+    // Convert an anonymous account to a permanent account
+    const facebookCredential = auth.FacebookAuthProvider.credential(token.accessToken);
+
+    const userCredential = await auth().currentUser?.linkWithCredential(facebookCredential);
+
+    // Request high quality profile picture from facebook and upload it to firebase storage.
+    const pictureUrl: string = await new Promise((resolve, reject) => {
+      // Request a higher quality profile picture
+      const infoRequest = new GraphRequest(
+        '/me',
+        {
+          accessToken: token.accessToken,
+          parameters: {
+            fields: {
+              string: 'picture.type(large)',
+            },
+          },
+        },
+        (error?: object, result?: GraphAPIResult) => {
+          if (error) {
+            crashlytics().log('Facebook profile picture request failed.');
+            reject(error);
+          } else {
+            const { url } = result!.picture.data;
+            resolve(url);
+          }
+        }
+      );
+
+      // Start the graph request.
+      new GraphRequestManager().addRequest(infoRequest).start();
+    });
+
+    await auth().currentUser?.updateProfile({
+      displayName: userCredential?.additionalUserInfo?.profile?.name,
+      photoURL: pictureUrl,
+    });
+
+    // TODO: Update pictureUrl & name in user document.
+    // return { ok: true, pictureUrl };
+  } catch (err) {
+    throw err;
+  }
+}
