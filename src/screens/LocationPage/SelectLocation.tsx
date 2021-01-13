ba7c9cc4ed32b0a28e1b8e8015033fcaedab2ec3
Guy Tepper <guytepper@gmail.com>
2021-01-13 10:15:23 +0200

    Update select location content

--- src/screens/LocationPage/SelectLocation.tsx
+++ src/screens/LocationPage/SelectLocation.tsx
@@ -73,11 +73,11 @@ function SelectLocation({ navigation }: SelectLocationScreenProps) {
 
       <Box alignItems="center" justifyContent="center" marginTop="xl">
         <Image source={require('@assets/illustrations/power-deluxe.png')} style={{ marginBottom: 16 }} />
-        <Text variant="extraLargeTitle" fontWeight={'900'} textAlign="center" color="lightText" marginBottom="s">
+        <Text variant="extraLargeTitle" textAlign="center" color="lightText" marginBottom="s">
           יצאתן להפגין? עשו צ׳ק אין!
         </Text>
         <Text variant="text" fontWeight={'400'} textAlign="center" color="lightText" marginBottom="xm">
-          ביחד נראה לכל הארץ כמה המחאה שלנו גדולה.
+          יחד נראה לכל הארץ כמה המחאה שלנו גדולה.
         </Text>
 
         {locationStore.nearbyLocations.length === 0 ? (
