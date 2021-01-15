41acfd2806273e42cb7e4d1c054890adde163e08
Guy Tepper <guytepper@gmail.com>
2021-01-15 12:32:05 +0200

    Location Counter fix + offline improvements (#79)

--- src/stores/EventStore.ts
+++ src/stores/EventStore.ts
@@ -2,6 +2,7 @@ import { makeAutoObservable, runInAction } from 'mobx';
 import firestore, { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
 import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 import EventsAPI from '../services/events';
+import { updateArrayByObjectId } from '@utils/array-utils';
 import { createAnonymousUser } from '../services/user';
 import { IEvent } from '@types/event';
 import rootStore from './RootStore';
@@ -27,10 +28,26 @@ class EventStore {
     }
   }
 
-  async attendEvent({ eventId, eventDate }: { eventId: string; eventDate: Date }): Promise<{ attended: boolean } | undefined> {
+  async attendEvent({
+    eventId,
+    attendingCount,
+    eventDate,
+  }: {
+    eventId: string;
+    attendingCount: number;
+    eventDate: Date;
+  }): Promise<{ attended: boolean } | undefined> {
     try {
-      const result = await EventsAPI.attendEvent({ eventId, eventDate });
+      // Add the event ID to the user events
       this.rootStore?.userStore.userEventIds.push(eventId);
+
+      const updatedEvents = updateArrayByObjectId(this.events, eventId, { attendingCount: attendingCount + 1 });
+
+      runInAction(() => {
+        this.events = updatedEvents;
+      });
+
+      const result = await EventsAPI.attendEvent({ eventId, eventDate });
 
       return result;
     } catch (err) {
@@ -38,18 +55,24 @@ class EventStore {
     }
   }
 
-  async unattendEvent({ eventId }: { eventId: string }): Promise<{ removed: boolean }> {
+  async unattendEvent({ eventId, attendingCount }: { eventId: string; attendingCount: number }): Promise<{ removed: boolean }> {
     try {
       let result;
       const { userEventIds } = this.rootStore!.userStore;
 
       if (userEventIds.includes(eventId)) {
-        result = await EventsAPI.unattendEvent({ eventId });
+        const updatedEvents = updateArrayByObjectId(this.events, eventId, { attendingCount: attendingCount - 1 });
+
+        runInAction(() => {
+          this.events = updatedEvents;
+        });
         const updatedUserEventIds = userEventIds.filter((id) => id !== eventId);
 
         runInAction(() => {
           this.rootStore!.userStore.userEventIds = updatedUserEventIds;
         });
+
+        result = await EventsAPI.unattendEvent({ eventId });
       } else {
         throw new Error('The eventId is missing from the user event Ids.');
       }
