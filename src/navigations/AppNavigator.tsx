86491baaad4a340b0ca6bd4eb588d03968266a30
Guy Tepper <guytepper@gmail.com>
2021-01-19 14:03:02 +0200

    Changed onboarding condition

--- src/navigations/AppNavigator.tsx
+++ src/navigations/AppNavigator.tsx
@@ -56,7 +56,7 @@ function AppNavigator() {
 
   return (
     <RootStack.Navigator screenOptions={{ stackPresentation: 'modal' }}>
-      {userStore.userData?.signupCompleted ? (
+      {userStore.user ? (
         <>
           <RootStack.Screen name="Main" component={MainStackScreen} options={{ headerShown: false }} />
           <RootStack.Screen name="CheckInModal" component={CheckInNavigator} options={{ headerShown: false }} />
