7c2d26aec06f993981386ab0dc0405b17ddbb27c
Guy Tepper <guytepper@gmail.com>
2021-01-23 14:26:11 +0200

    Added new port “post” button

--- src/screens/NewPost/NewPost.tsx
+++ src/screens/NewPost/NewPost.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, Image, StyleSheet, Dimensions } from 'react-native';
+import { ScrollView, Image, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
 import { Box, Text } from '../../components';
 import FastImage from 'react-native-fast-image';
 import { TextInput } from 'react-native-gesture-handler';
@@ -10,8 +10,22 @@ const deviceWidth = Dimensions.get('window').width;
 const profilePicture =
   'https://scontent.ftlv15-1.fna.fbcdn.net/v/t1.0-9/120795507_338405427579471_6909790557627558055_o.jpg?_nc_cat=111&ccb=2&_nc_sid=09cbfe&_nc_ohc=PQG6dprSZCIAX8VJfTJ&_nc_ht=scontent.ftlv15-1.fna&oh=fc18d3204ba2333145b39aad27c838ae&oe=6032F1FF';
 
-function NewPost({ route }: NewPostProps) {
+function NewPost({ navigation, route }: NewPostProps) {
   const { image } = route.params;
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity activeOpacity={0.8}>
+          <Box backgroundColor="primaryColor" paddingHorizontal="m" paddingVertical="xs" marginBottom="s" borderRadius={4}>
+            <Text variant="text" fontSize={18} color="primaryText" fontWeight="600">
+              פרסום
+            </Text>
+          </Box>
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
 
   return (
     <ScrollView style={{ paddingTop: 12 }}>
