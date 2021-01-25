9254147741a49aa05c61745d6a173fad334019b9
Guy Tepper <guytepper@gmail.com>
2021-01-25 16:23:04 +0200

    Update PictureThumbList.tsx

--- src/components/PictureThumbList/PictureThumbList.tsx
+++ src/components/PictureThumbList/PictureThumbList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Dimensions, StyleSheet } from 'react-native';
 import FastImage from 'react-native-fast-image';
 import { IPicturePost } from '@types/post';
@@ -13,16 +13,54 @@ const deviceHeight = Dimensions.get('window').height;
 const thumbSize = deviceWidth / 3;
 const placeholderThumbSize = thumbSize - 2;
 
+const placeholderThumb = (index: number, loadedImages: number[]) => {
+  if (loadedImages.includes(index)) return null;
+
+  // Default x & y values for the first thumb in the row.
+  let x = 1;
+  let y = 1;
+
+  // For the second thumb in the row.
+  if (index % 3 === 2) {
+    x = thumbSize + 1; // +1 to cover the image thumb stroke.
+  }
+
+  // 3rd thumb in the row.
+  if (index % 3 === 0) {
+    // +1 to cover the image thumb stroke.
+    x = thumbSize * 2 + 1;
+  }
+
+  // Get the current row index.
+  const row = Math.floor(index / 3 - 0.01); // 0.01 is a hack to revert full numbers to the previous line.
+
+  if (row > 0) {
+    y = thumbSize * row + 1; // Set the y axis to accomodate the thumbSize and the stroke
+  }
+
+  if (index === 3) {
+    return null;
+  }
+  return <Rect x={x} y={y} width={placeholderThumbSize} height={placeholderThumbSize} key={index} />;
+};
+
 function PictureThumbList({ pictures }: { pictures: IPicturePost[] }) {
   const navigation = useNavigation();
-
+  const [loadedImages, setLoadedImages] = useState<number[]>([]);
+  console.log(loadedImages);
   return (
-    <Box flexDirection="row" flexWrap="wrap">
-      {pictures.map((picture, index) => (
-        <TouchableOpacity onPress={() => navigation.navigate('ExploreList', { initialIndex: index })} key={picture.id}>
-          <FastImage style={styles.imageThumb} source={{ uri: picture.pictureUrl }} />
-        </TouchableOpacity>
-      ))}
+    <Box>
+      <Box flexDirection="row" flexWrap="wrap" position="absolute" zIndex={3}>
+        {pictures.map((picture, index) => (
+          <TouchableOpacity onPress={() => navigation.navigate('ExploreList', { initialIndex: index })} key={picture.id}>
+            <FastImage
+              style={styles.imageThumb}
+              source={{ uri: picture.pictureUrl }}
+              onLoad={() => setLoadedImages((prevState) => [...prevState, index])}
+            />
+          </TouchableOpacity>
+        ))}
+      </Box>
 
       <ContentLoader
         width={deviceWidth}
@@ -32,24 +70,7 @@ function PictureThumbList({ pictures }: { pictures: IPicturePost[] }) {
         rtl
         style={styles.contentLoaderWrapper}
       >
-        <Rect x="1" y="0" width={placeholderThumbSize} height={thumbSize} strokeWidth={1} stroke="#fff" />
-        <Rect x={thumbSize + 1} y="0" width={placeholderThumbSize} height={thumbSize} />
-        <Rect x={thumbSize * 2 + 1} y="0" width={placeholderThumbSize} height={thumbSize} />
-        <Rect x="1" y={thumbSize + 1} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x={thumbSize + 1} y={thumbSize + 1} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x={thumbSize * 2 + 1} y={thumbSize + 1} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x="1" y={thumbSize * 2 + 2} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x={thumbSize + 1} y={thumbSize * 2 + 2} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x={thumbSize * 2 + 1} y={thumbSize * 2 + 2} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x="1" y={thumbSize * 3 + 3} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x={thumbSize + 1} y={thumbSize * 3 + 3} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x={thumbSize * 2 + 1} y={thumbSize * 3 + 3} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x="1" y={thumbSize * 4 + 4} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x={thumbSize + 1} y={thumbSize * 4 + 4} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x={thumbSize * 2 + 1} y={thumbSize * 4 + 4} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x="1" y={thumbSize * 5 + 5} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x={thumbSize + 1} y={thumbSize * 5 + 5} width={placeholderThumbSize} height={thumbSize} />
-        <Rect x={thumbSize * 2 + 1} y={thumbSize * 5 + 5} width={placeholderThumbSize} height={thumbSize} />
+        {pictures.map((_, index) => placeholderThumb(index + 1, loadedImages))}
       </ContentLoader>
     </Box>
   );
@@ -60,6 +81,7 @@ export default PictureThumbList;
 const styles = StyleSheet.create({
   contentLoaderWrapper: {
     position: 'absolute',
+    zIndex: 0,
   },
   imageThumb: {
     height: thumbSize,
@@ -67,3 +89,24 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
 });
+
+{
+  /* <Rect x="1" y="0" width={placeholderThumWidth} height={placeholderThumHeight} strokeWidth={1} stroke="#fff" />
+        <Rect x={thumbSize + 1} y="0" width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x={thumbSize * 2 + 1} y="0" width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x="1" y={thumbSize + 1} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x={thumbSize + 1} y={thumbSize + 1} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x={thumbSize * 2 + 1} y={thumbSize + 1} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x="1" y={thumbSize * 2 + 2} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x={thumbSize + 1} y={thumbSize * 2 + 2} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x={thumbSize * 2 + 1} y={thumbSize * 2 + 2} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x="1" y={thumbSize * 3 + 3} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x={thumbSize + 1} y={thumbSize * 3 + 3} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x={thumbSize * 2 + 1} y={thumbSize * 3 + 3} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x="1" y={thumbSize * 4 + 4} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x={thumbSize + 1} y={thumbSize * 4 + 4} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x={thumbSize * 2 + 1} y={thumbSize * 4 + 4} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x="1" y={thumbSize * 5 + 5} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x={thumbSize + 1} y={thumbSize * 5 + 5} width={placeholderThumWidth} height={placeholderThumHeight} />
+        <Rect x={thumbSize * 2 + 1} y={thumbSize * 5 + 5} width={placeholderThumWidth} height={placeholderThumHeight} /> */
+}
