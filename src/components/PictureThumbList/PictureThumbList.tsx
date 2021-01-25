e165abd921cdfde8152a5755bc93d7bd67d08dab
Guy Tepper <guytepper@gmail.com>
2021-01-25 11:52:42 +0200

    Improve loader layout

--- src/components/PictureThumbList/PictureThumbList.tsx
+++ src/components/PictureThumbList/PictureThumbList.tsx
@@ -11,35 +11,45 @@ const tempPics = Array.from(Array(30).keys());
 const deviceWidth = Dimensions.get('window').width;
 const deviceHeight = Dimensions.get('window').height;
 const thumbSize = deviceWidth / 3;
+const placeholderThumbSize = thumbSize - 2;
 
 function PictureThumbList({ pictures }: { pictures: IPicturePost[] }) {
   const navigation = useNavigation();
 
   return (
     <Box flexDirection="row" flexWrap="wrap">
-      {/* {pictures.map((picture, index) => (
+      {pictures.map((picture, index) => (
         <TouchableOpacity onPress={() => navigation.navigate('ExploreList', { initialIndex: index })} key={picture.id}>
           <FastImage style={styles.imageThumb} source={{ uri: picture.pictureUrl }} />
         </TouchableOpacity>
-      ))} */}
-      {/* {tempPics.map(() => (
-        <Box backgroundColor="sectionListSeperator" height={thumbSize} width={thumbSize} borderWidth={1}>
-          <ContentLoader height={thumbSize} width={thumbSize} backgroundColor="#222222" foregroundColor="#333333" rtl>
-            <Rect x="0" y="0" rx="3" ry="3" width="300" height="300" />
-          </ContentLoader>
-        </Box>
-      ))} */}
+      ))}
 
-      <ContentLoader width={deviceWidth} height={deviceHeight} backgroundColor="#222222" foregroundColor="#333333" rtl>
-        <Rect x="0" y="0" rx="2" ry="2" width={thumbSize} height={thumbSize} />
-        <Rect x={thumbSize + 1} y="0" rx="2" ry="2" width={thumbSize} height={thumbSize} />
-        <Rect x={thumbSize * 2 + 2} y="0" rx="2" ry="2" width={thumbSize} height={thumbSize} />
-        <Rect x="0" y={thumbSize + 1} rx="2" ry="2" width={thumbSize} height={thumbSize} />
-        <Rect x={thumbSize + 1} y={thumbSize + 1} rx="2" ry="2" width={thumbSize} height={thumbSize} />
-        <Rect x={thumbSize * 2 + 2} y={thumbSize + 1} rx="2" ry="2" width={thumbSize} height={thumbSize} />
-        <Rect x="0" y={thumbSize * 2 + 2} rx="2" ry="2" width={thumbSize} height={thumbSize} />
-        <Rect x={thumbSize + 1} y={thumbSize * 2 + 2} rx="2" ry="2" width={thumbSize} height={thumbSize} />
-        <Rect x={thumbSize * 2 + 2} y={thumbSize * 2 + 2} rx="2" ry="2" width={thumbSize} height={thumbSize} />
+      <ContentLoader
+        width={deviceWidth}
+        height={deviceHeight}
+        backgroundColor="#222222"
+        foregroundColor="#333333"
+        rtl
+        style={styles.contentLoaderWrapper}
+      >
+        <Rect x="1" y="0" width={placeholderThumbSize} height={thumbSize} strokeWidth={1} stroke="#fff" />
+        <Rect x={thumbSize + 1} y="0" width={placeholderThumbSize} height={thumbSize} />
+        <Rect x={thumbSize * 2 + 1} y="0" width={placeholderThumbSize} height={thumbSize} />
+        <Rect x="1" y={thumbSize + 1} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x={thumbSize + 1} y={thumbSize + 1} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x={thumbSize * 2 + 1} y={thumbSize + 1} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x="1" y={thumbSize * 2 + 2} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x={thumbSize + 1} y={thumbSize * 2 + 2} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x={thumbSize * 2 + 1} y={thumbSize * 2 + 2} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x="1" y={thumbSize * 3 + 3} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x={thumbSize + 1} y={thumbSize * 3 + 3} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x={thumbSize * 2 + 1} y={thumbSize * 3 + 3} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x="1" y={thumbSize * 4 + 4} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x={thumbSize + 1} y={thumbSize * 4 + 4} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x={thumbSize * 2 + 1} y={thumbSize * 4 + 4} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x="1" y={thumbSize * 5 + 5} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x={thumbSize + 1} y={thumbSize * 5 + 5} width={placeholderThumbSize} height={thumbSize} />
+        <Rect x={thumbSize * 2 + 1} y={thumbSize * 5 + 5} width={placeholderThumbSize} height={thumbSize} />
       </ContentLoader>
     </Box>
   );
@@ -48,6 +58,9 @@ function PictureThumbList({ pictures }: { pictures: IPicturePost[] }) {
 export default PictureThumbList;
 
 const styles = StyleSheet.create({
+  contentLoaderWrapper: {
+    position: 'absolute',
+  },
   imageThumb: {
     height: thumbSize,
     width: thumbSize,
