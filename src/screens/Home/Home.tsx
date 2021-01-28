6a58139b9306e67653f259e4eccf544debd11fc8
Guy Tepper <guytepper@gmail.com>
2021-01-28 21:01:35 +0200

    Added image gallery

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { observer } from 'mobx-react-lite';
-import { Box, Text } from '../../components';
-import { View, StatusBar, StyleSheet, ScrollView } from 'react-native';
+import { Box, Text, CircularButton } from '../../components';
+import { Modal, StatusBar, StyleSheet, ScrollView, Button, Pressable } from 'react-native';
 import Stats from './Widgets/Stats';
 import FeaturedEvents from './Widgets/FeaturedEvents';
+import ImageViewer from 'react-native-image-zoom-viewer';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const images = [
+  {
+    url: 'https://placekitten.com/300/300',
+  },
+  {
+    url: 'https://placekitten.com/600/300',
+  },
+];
 
 function Home() {
+  const [displayGallery, setDisplayGallery] = useState(false);
+
   return (
     <ScrollView style={styles.homeWrapper}>
       <StatusBar backgroundColor="#0a0a0a" barStyle="light-content" networkActivityIndicatorVisible={false} />
@@ -16,6 +29,28 @@ function Home() {
         </Text>
       </Box>
       <FeaturedEvents />
+      <Box paddingHorizontal="m" marginTop="m">
+        <Text variant="largeTitle" color="lightText">
+          תמונות נבחרות
+        </Text>
+      </Box>
+      <Button title="hiellow" onPress={() => setDisplayGallery(true)} />
+
+      <Modal visible={displayGallery} transparent={true} animationType="slide">
+        <ImageViewer
+          renderIndicator={() => null}
+          imageUrls={images}
+          useNativeDriver={true}
+          index={0}
+          enableSwipeDown={true}
+          swipeDownThreshold={35}
+          saveToLocalByLongPress={false}
+          onCancel={() => setDisplayGallery(false)}
+        />
+        <Box position="absolute" top={35} left={15}>
+          <CircularButton onPress={() => setDisplayGallery(false)} iconName="x" color="white" size="small" />
+        </Box>
+      </Modal>
     </ScrollView>
   );
 }
