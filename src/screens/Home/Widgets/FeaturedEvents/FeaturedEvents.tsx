a3766af4c33da75e05f2e4950aa4932425e962a8
Guy Tepper <guytepper@gmail.com>
2021-01-31 10:46:26 +0200

    Added 1st event loader

--- src/screens/Home/Widgets/FeaturedEvents/FeaturedEvents.tsx
+++ src/screens/Home/Widgets/FeaturedEvents/FeaturedEvents.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, ViewStyle } from 'react-native';
+import { ScrollView, StyleSheet, ViewStyle, Dimensions } from 'react-native';
 import { observer } from 'mobx-react-lite';
 import analytics from '@react-native-firebase/analytics';
 import { useNavigation } from '@react-navigation/native';
@@ -7,6 +7,9 @@ import { useStore } from '../../../../stores';
 import { Box } from '../../../../components';
 import { IEvent } from '@types/event';
 import EventCompactBox from './EventCompactBox';
+import ContentLoader, { Rect } from 'react-content-loader/native';
+
+const { width: deviceWidth, height: deviceHeight } = Dimensions.get('window');
 
 type EventsWidgetProps = {
   style?: ViewStyle;
@@ -23,6 +26,15 @@ function EventsWidget({ style }: EventsWidgetProps) {
 
   return (
     <Box style={style} flex={1} width="100%">
+      <Box paddingHorizontal="m">
+        <ContentLoader width={deviceWidth} height={deviceHeight} backgroundColor="#222222" foregroundColor="#333333" rtl>
+          <Rect x="0" y={0} rx={8} width={154} height={180} />
+          <Rect x="3" y={187} width={90} height={12} />
+          <Rect x="3" y={204} width={135} height={12} />
+          <Rect x="3" y={222} width={108} height={12} />
+        </ContentLoader>
+      </Box>
+
       <ScrollView contentContainerStyle={styles.featuredEvents} showsHorizontalScrollIndicator={false} horizontal={true}>
         {eventStore.events.slice(0, 5).map((event: IEvent, index: number) => (
           <EventCompactBox {...event} onPress={() => onEventPress(event.id, index)} key={event.id} />
