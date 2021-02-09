10a40badc431c143cc179dad6320fb2291acebff
Guy Tepper <guytepper@gmail.com>
2021-02-09 08:53:47 +0200

    Moved home titles box styles to text component

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -2,25 +2,29 @@ import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { Box, Text } from '../../components';
 import { StatusBar, StyleSheet, ScrollView } from 'react-native';
-import { Stats, FeaturedEvents, FeaturedPictures } from '@components/Widgets';
+import { Stats, FeaturedPictures, FeaturedEvents, FeaturedProtests } from '@components/Widgets';
 
 function Home() {
   return (
     <ScrollView style={styles.homeWrapper}>
       <StatusBar backgroundColor="#0a0a0a" barStyle="light-content" networkActivityIndicatorVisible={false} />
       <Stats />
-      <Box paddingHorizontal="m" marginTop="m" marginBottom="xm">
-        <Text variant="largeTitle" color="lightText">
-          תמונות נבחרות
-        </Text>
-      </Box>
+
+      <Text variant="largeTitle" color="lightText" paddingHorizontal="m" marginTop="m" marginBottom="xm">
+        תמונות נבחרות
+      </Text>
+
       <FeaturedPictures style={{ marginBottom: 12 }} />
 
-      <Box paddingHorizontal="m" marginTop="m" marginBottom="xm">
-        <Text variant="largeTitle" color="lightText">
-          הפגנות קרובות
-        </Text>
-      </Box>
+      <Text variant="largeTitle" color="lightText" paddingHorizontal="m" marginTop="m" marginBottom="xm">
+        עכשיו מפגינים
+      </Text>
+
+      <FeaturedProtests />
+
+      <Text variant="largeTitle" color="lightText" paddingHorizontal="m" marginTop="m" marginBottom="xm">
+        הפגנות קרובות
+      </Text>
 
       <FeaturedEvents style={{ marginBottom: 42 }} />
     </ScrollView>
