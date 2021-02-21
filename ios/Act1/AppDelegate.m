532be33ab2409745fae49c839a3e30a2a2a14029
Guy Tepper <guytepper@gmail.com>
2021-02-21 10:29:34 +0200

    Configure react-native-unimodules for iOS

--- ios/Act1/AppDelegate.m
+++ ios/Act1/AppDelegate.m
@@ -7,6 +7,10 @@
 #import <React/RCTRootView.h>
 #import <React/RCTI18nUtil.h> 
 
+#import <UMCore/UMModuleRegistry.h>
+#import <UMReactNativeAdapter/UMNativeModulesProxy.h>
+#import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
+
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 #import "RNBootSplash.h"
@@ -30,6 +34,12 @@ static void InitializeFlipper(UIApplication *application) {
 }
 #endif
 
+@interface AppDelegate () <RCTBridgeDelegate>
+ 
+@property (nonatomic, strong) UMModuleRegistryAdapter *moduleRegistryAdapter;
+ 
+@end
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -45,6 +55,8 @@ static void InitializeFlipper(UIApplication *application) {
   [[RCTI18nUtil sharedInstance] forceRTL:YES];[[RCTI18nUtil sharedInstance] forceRTL:YES];
   [[FBSDKApplicationDelegate sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
 
+  self.moduleRegistryAdapter = [[UMModuleRegistryAdapter alloc] initWithModuleRegistryProvider:[[UMModuleRegistryProvider alloc] init]];
+
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"Act1"
@@ -56,9 +68,17 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
 
   [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView];
   return YES;
+}
+
+- (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge
+{
+    NSArray<id<RCTBridgeModule>> *extraModules = [_moduleRegistryAdapter extraModulesForBridge:bridge];
+    // If you'd like to export some custom RCTBridgeModules that are not Expo modules, add them here!
+    return extraModules;
 }
 
 - (BOOL)application:(UIApplication *)app
