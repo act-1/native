2f9a2ad6a5890765941bd09ca3ac86f7ba55443b
Guy Tepper <guytepper@gmail.com>
2021-03-16 08:48:12 +0200

    Remove Explore components

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -3,5 +3,4 @@ export { default as EventsNavigator } from './EventsNavigator';
 export { default as CheckInNavigator } from './CheckInNavigator';
 export { default as ActionNavigator } from './CheckInNavigator';
 export { default as ProfileNavigator } from './ProfileNavigator';
-export { default as ExploreNavigator } from './ExploreNavigator';
 export { default as CommunityNavigator } from './CommunityNavigator';
--- src/screens/Explore/index.ts
+++
@@ -1,5 +0,0 @@
-import Explore from './Explore';
-export { default as Explore } from './Explore';
-export { default as ExploreList } from './ExploreList';
-
-export default Explore;
